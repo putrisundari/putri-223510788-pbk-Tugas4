c11e134203d9023e8e9fb99f03f2915e440defdb
putrisundari <163516849+putrisundari@users.noreply.github.com>
2024-05-15 20:06:35 +0700

    Add files via upload

---
+++ src/components/childtwo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="child">
+      <h3>Child Two</h3>
+      <button @click="sendData">Klik Tombol Ini</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'childtwo',
+    emits: ['data-from-child'],
+    setup(props, { emit }) {
+      const sendData = () => {
+        emit('data-from-child', 'Data from child two');
+      };
+  
+      return {
+        sendData,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .child {
+    border: 1px solid rgb(123,123,158);
+    padding: 10px;
+    margin: 10px;
+    text-align: center;
+  }
+  button {
+    background-color: rgb(115, 115,158);
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+  button:hover {
+    background-color: rgb(95, 95, 140);
+  }
+  </style>
+  
---
+++ src/components/parent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <h2>Parent Component</h2>
+      <p>Data from Child: {{ childData }}</p>
+      <ChildOne @data-from-child="handleDataFromChild" />
+      <ChildTwo @data-from-child="handleDataFromChild" />
+    </div>
+  </template>
+  
+  <script>
+  import ChildOne from './childone.vue';
+  import ChildTwo from './childtwo.vue';
+  
+  export default {
+    name: 'Parent',
+    components: {
+      ChildOne,
+      ChildTwo
+    },
+    data() {
+      return {
+        childData: ''
+      };
+    },
+    methods: {
+      handleDataFromChild(data) {
+        this.childData = data;
+      }
+    }
+  }
+  </script>
